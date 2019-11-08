f81b8bc3b5d7d2f5a70751748aa8c1f92d87310b
Andre Rodrigues Melo <andre.melo@softtek.com>
2019-11-08 09:39:07 -0300

    escutar por variável de ambiente port em maiúsculo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose
 
 app.use(express.json())
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`api funcionando na porta ${port}`))
 
 app.post("/api/usuarios", async (req, res) => {
