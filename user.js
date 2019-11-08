bb1219d28d96f9ae6efcfabd3e011f6ac60d0858
arthur.gomes <tuca326112>
2019-11-08 17:41:59 -0300

    teste

--- user.js
+++ user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     validate: {
       validator: function() {
         const nome = this.nome.split(" ")
-        return nome.length < 2
+        return nome.length >= 3
       },
       message: "O nome deve ser completo"
     }
