d8b0e3e60667b04223a60fb554e2a2aac3ff1fe6
André Rodrigues Melo <andre@pop-os.localdomain>
2019-11-07 23:34:38 -0300

    Alterar uri do banco

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const mongoose = require('mongoose')
-const User = require('./user')
+const mongoose = require("mongoose")
+const User = require("./user")
 
 console.log(process.env.mongoDBUri)
 
 mongoose
-.connect(process.env.mongoDBUri)
-// .connect('mongodb://localhost')//quando precisar testar localmente
-.then(() => console.log('Conectado ao Mongodb...'))
-.catch( error => console.error('Não foi possível se comunicar ao Mongodb...'))
+  .connect("mongodb://arthuruchoa:tuca326112@ds141188.mlab.com:41188/adforms")
+  // .connect('mongodb://localhost')//quando precisar testar localmente
+  .then(() => console.log("Conectado ao Mongodb..."))
+  .catch(error => console.error("Não foi possível se comunicar ao Mongodb..."))
 
 app.use(express.json())
 
 const port = process.env.port || 3000
 app.listen(port, () => console.log(`api funcionando na porta ${port}`))
 
-app.post('/api/usuarios', async (req, res) => {
-    const newUser = new User(req.body)
+app.post("/api/usuarios", async (req, res) => {
+  const newUser = new User(req.body)
 
-    const userResult = await newUser.save()
+  const userResult = await newUser.save()
 
-    if (userResult.message) {
-        return res.status(400).send({
-            "message": userResult.message
-        })
-    } else {
-        return res.status(200).send({
-            "message": "Usuário salvo com sucesso!"
-        })
-    }
-
+  if (userResult.message) {
+    return res.status(400).send({
+      message: userResult.message
+    })
+  } else {
+    return res.status(200).send({
+      message: "Usuário salvo com sucesso!"
+    })
+  }
 })
